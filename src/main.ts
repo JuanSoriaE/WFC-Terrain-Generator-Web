198b5febce3c922c4af6b7b1ba7397a512d02211
JuanSoriaE <jsoriae1700@alumno.ipn.mx>
2023-07-12 17:15:51 -0600

    Fixed: CSS in deployment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import Generator from "./components/Generator";
+import './styles/style.sass';
+import './styles/main.sass';
+import './styles/wfc_params.sass';
+import './styles/states_settings.sass';
+import './styles/mid_panel.sass';
+import './styles/image_settings.sass';
+import './styles/imgs-modal.sass';
+import './styles/D3_settings.sass';
 
-const generator: Generator = new Generator();
+let generator: Generator;
 
 function main() {
-  
+  generator = new Generator();
 }
 
 main();
